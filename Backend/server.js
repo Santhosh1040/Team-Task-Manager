6f1f618f6415352d1a850fbd44f7970e6e750a53
Santhosh1040 <santhosh22462246@gmail.com>
2026-05-21 01:29:59 +0530

    final cors fix

--- Backend/server.js
+++ Backend/server.js
@@ -4,20 +4,29 @@ require("dotenv").config();
 
 const authRoutes = require("./routes/authRoutes");
 const projectRoutes = require("./routes/projectRoutes");
-const app = express();
 const taskRoutes = require("./routes/taskRoutes");
 
+const app = express();
+
+// CORS
 app.use(
   cors({
-    origin: "https://team-task-manager-1qbnm9ih4-santhoshs-projects-1fd6dff9.vercel.app",
+    origin: [
+      "https://team-task-manager-1qbnm9ih4-santhoshs-projects-1fd6dff9.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
+
 app.use(express.json());
 
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/projects", projectRoutes);
 app.use("/api/tasks", taskRoutes);
+
+// Test Route
 app.get("/", (req, res) => {
   res.send("API Running");
 });
